f2fdc59a1f1031cc12fd68f535340f5c677b18d3
Michel Krapf <michel.krapf@gmail.com>
2023-02-14 22:28:11 +0100

    image rotation

--- script/scrpt.ps1
+++ script/scrpt.ps1
@@ -19,7 +19,24 @@ add-type $code
 
 # Continuously download a new wallpaper and set it as the desktop background every hour
 while ($true) {
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mkrapf/AMWP/master/images/active.jpg" -OutFile $imgPath
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mkrapf/AMWP/master/images/babygorilla.jpg" -OutFile $imgPath
     [Win32.Wallpaper]::SetWallpaper($imgPath)
     Start-Sleep -Seconds 300
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mkrapf/AMWP/master/images/confusedbaby.jpg" -OutFile $imgPath
+    [Win32.Wallpaper]::SetWallpaper($imgPath)
+    Start-Sleep -Seconds 300
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mkrapf/AMWP/master/images/dicknose.jpg" -OutFile $imgPath
+    [Win32.Wallpaper]::SetWallpaper($imgPath)
+    Start-Sleep -Seconds 300
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mkrapf/AMWP/master/images/grinning.jpg" -OutFile $imgPath
+    [Win32.Wallpaper]::SetWallpaper($imgPath)
+    Start-Sleep -Seconds 300
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mkrapf/AMWP/master/images/scary.jpg" -OutFile $imgPath
+    [Win32.Wallpaper]::SetWallpaper($imgPath)
+    Start-Sleep -Seconds 300
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/mkrapf/AMWP/master/images/sitting.jpg" -OutFile $imgPath
+    [Win32.Wallpaper]::SetWallpaper($imgPath)
+    Start-Sleep -Seconds 300
+
+
 } 
